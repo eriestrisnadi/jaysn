45b76b08f79a9a1e49bd889af9b0d88ec28d6bb9
Eries Trisnadi <lognus21@gmail.com>
2021-02-07 17:41:40 +0700

    refactor: change code to be more clearer
    Signed-off-by: Eries Trisnadi <lognus21@gmail.com>

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+export const JAYSN_PATH = '.jaysn';
+
+export const IMMUTABLE_TYPE = [
+  'List',
+  'Map',
+  'OrderedMap',
+  'Set',
+  'OrderedSet',
+  'Stack',
+  'Range',
+  'Repeat',
+  'Record',
+  'Seq',
+  'Collection',
+];
+
+export const IMMUTABLE_READ = ['get', 'getIn'];
+
+export const IMMUTABLE_WRITE = [
+  'delete',
+  'deleteIn',
+  'merge',
+  'mergeDeep',
+  'mergeDeepIn',
+  'mergeIn',
+  'remove',
+  'removeIn',
+  'set',
+  'setIn',
+  'update',
+  'updateIn',
+];
+
+export default {
+  JAYSN_PATH,
+  IMMUTABLE_TYPE,
+  IMMUTABLE_READ,
+  IMMUTABLE_WRITE,
+};
